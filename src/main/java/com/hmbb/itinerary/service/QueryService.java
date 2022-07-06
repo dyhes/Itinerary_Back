63ec2dbfc48aa648928e2266ac4bae507c2c1995
LBWyes <88589340+LBWyes@users.noreply.github.com>
2022-07-06 23:49:33 +0800

    形成推荐(没有全部完成)

--- src/main/java/com/hmbb/itinerary/service/QueryService.java
+++ src/main/java/com/hmbb/itinerary/service/QueryService.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sun.awt.geom.AreaOp;
 
+import javax.swing.text.Segment;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -32,6 +34,7 @@ public class QueryService {
 
     public QueryResult query(QueryParam queryParam) {
         int passenger = queryParam.getPassenger();
+        //这个是分段数目
         int segment = queryParam.getSegment();
         int result = queryParam.getResult();
         List<String> departureDates =  queryParam.getDepartureDate();
@@ -51,21 +54,21 @@ public class QueryService {
         journey.add(new JourneyParam(agent,1234,seg));
         return new QueryResult(1,journey);*/
 
-        List<List<TSeat>> flights = new ArrayList<>();
+  //      List<List<TSeat>> flights = new ArrayList<>();
+        List<SegmentParam> segmentParams = new ArrayList<>();
         //对每段航班进行操作
         for(int i=0;i<segment;i++){
-            //1.挑出来符合日期、出发地和目的地且余票充足的航班，这一步只需要在TSeat表中查询即可完成
-            List<TSeat> flight = getAppropriateFlights(departureDates.get(i),departures.get(i),arrivals.get(i),passenger);
-
-            //2.对上面的flight进行处理，得到每个乘客cabin信息，优先级Y>C>F
-
+            //1.挑出来符合日期、出发地和目的地航班，这一步只需要在TSeat表中查询即可完成
+            List<TSeat> flight = getAppropriateFlights(departureDates.get(i),departures.get(i),arrivals.get(i));
+            //2.对上面的flight进行处理，筛选掉余票不足的并得到每个乘客cabin信息，优先级Y>C>F
+            segmentParams = filterSeat(flight,passenger);
             //3.得到基础票价
 
             //4.去除flight中代理商不对的航班，并保存其下一段航班
 
 
             //保存该段的航班
-            flights.add(flight);
+    //        flights.add(flight);
         }
 
         //得到最后的输出：合并同价代理agent，计算同result多segment的总票价
@@ -73,8 +76,51 @@ public class QueryService {
         return null;
     }
 
-    private List<TSeat> getAppropriateFlights(String departureDate, String departure, String arrival, int passenger){
-        //List<TFlight> flights = seatRepository;
-        return null;
+    private List<TSeat> getAppropriateFlights(String departureDate, String departure, String arrival){
+        return seatRepository.getAppropriateFlights(departureDate,departure, arrival);
+    }
+
+    private List<SegmentParam> filterSeat(List<TSeat> flight,int passenger){
+        List<String> cabin = new ArrayList<>();
+        List<SegmentParam> segmentParams = new ArrayList<>();
+        int size = flight.size();
+        for(int i = size - 1; i >= 0; i--){
+
+            TSeat item = flight.get(i);
+
+            int seatY = Integer.parseInt(item.getSeatY());
+            int seatC = Integer.parseInt(item.getSeatC());
+            int seatF = Integer.parseInt(item.getSeatF());
+
+            if(seatY>=passenger){
+                for(int h=0;h<passenger;h++){
+                    cabin.add("Y");
+                }
+            }else if(seatY+seatC>=passenger){
+                for(int h=0;h<seatY;h++){
+                    cabin.add("Y");
+                }
+                for(int h=0;h<passenger-seatY;h++){
+                    cabin.add("C");
+                }
+            }else if(seatY+seatC+seatF>=passenger){
+                for(int h=0;h<seatY;h++){
+                    cabin.add("Y");
+                }
+                for(int h=0;h<seatC;h++){
+                    cabin.add("C");
+                }
+                for(int h=0;h<passenger-seatY-seatF;h++){
+                    cabin.add("F");
+                }
+            }else{
+                //加起来都不够，舍去
+                flight.remove(item);
+                continue;
+            }
+
+            segmentParams.add(new SegmentParam(item,cabin));
+        }
+        return segmentParams;
     }
 }
